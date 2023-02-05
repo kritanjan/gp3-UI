d715b2d23598708dc6987a1c6ecf7c889e189eba
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2023-02-06 02:34:57 +0530

    Possibility Component Completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 const Menu = () => {
   return (
     <><p><a href="#home">Home</a></p>
-      <p><a href="#wgpt3">What is GPT3</a></p>
+      <p><a href="#whatgpt3">What is GPT3</a></p>
       <p><a href="#possibility">Open AI</a></p>
       <p><a href="#features">Case Studies</a></p>
-      <p><a href="blog">Library</a></p></>
+      <p><a href="#blog">Library</a></p></>
   )
 }
 
