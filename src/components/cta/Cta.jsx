40fe14a0c15fb8884a58863108c85b253a919767
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2023-02-06 02:57:48 +0530

    completed the cta section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 function Cta() {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta section__margin'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button>Get Started</button>
+      </div>
+    </div>
   )
 }
 
